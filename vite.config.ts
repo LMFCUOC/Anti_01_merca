2d2f5ec3592c2c5576d6965d2721eb80e6bfabc0
LMFCUOC <104617476+LMFCUOC@users.noreply.github.com>
2025-12-02 16:10:26 +0100

    feat: add GitHub Pages deployment workflow and configure Vite base path.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mercadona-flow/', // Cambia 'mercadona-flow' por el nombre de tu repositorio
   plugins: [react()],
   resolve: {
     alias: {
